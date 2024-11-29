1fd60911369bb8e09ed4dd1866aeca34ee9c0d0c
Easy lua <staskuzmin1406@gmail.com>
2024-11-29 16:16:27 +0300

    Update Privatesrc.lua

--- Privatesrc.lua
+++ Privatesrc.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Player01010111Cheater/Testing/refs/heads/main/testing.txt"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Player01010111Cheater/Testing/refs/heads/main/testing.txt"))()
